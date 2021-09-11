c921b23bea17cf0f1f9e6c71875e0b995f7595ed
MyungHoon Jin <jinmang2@gmail.com>
2021-09-12 04:07:58 +0900

    Fix

--- main.py
+++ main.py
@@ -4,11 +4,5 @@ def hello_world(name):
     return msg
 
 
-def hello_world(name):
-    msg = f"Hello World! {name}!"
-    print(msg)
-    return msg
-    
-
 if __name__ == "__main__":  # pragma: no cover
     hello_world("jinmang2")
--- main_test.py
+++ main_test.py
@@ -9,5 +9,5 @@ class MainTest(unittest.TestCase):
         self.assertEqual(ret, "Hello World! Test!")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
