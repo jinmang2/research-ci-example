ac5f56792b9d9ee022cf12940f48b9d4c8bbe527
MyungHoon Jin <jinmang2@gmail.com>
2021-09-12 04:06:12 +0900

    make code duplicate

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ def hello_world(name):
     return msg
 
 
+def hello_world(name):
+    msg = f"Hello World! {name}!"
+    print(msg)
+    return msg
+    
+
 if __name__ == "__main__":  # pragma: no cover
     hello_world("jinmang2")
